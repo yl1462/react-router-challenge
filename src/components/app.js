9a2062fbde6b3bdf66dceb4e99ed9b346d1785fe
Yuri Liang <68250067+yl1462@users.noreply.github.com>
2020-12-20 22:13:47 -0600

    this is what they approved

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import AboutMenu from './about-menu';
+import {Route} from 'react-router-dom'
 
 export default function App(props) {
     return (
         <div className="app">
             {/* This should only show up on /about/foo, /about/bar, or /about/foo/bar */}
-            <AboutMenu />
+            <Route path="/about/:dynamic"><AboutMenu /></Route>
         </div>
     );
 }
